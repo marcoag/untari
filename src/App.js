82df904d522787864c799f1ad560b20feb92b28c
untari <sastraembun0306@gmail.com>
2020-06-12 06:45:26 +0700

    adding build static

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Blog from './Components/Blog';
-import Navigation from './Components/Navigation';
+
  
 class App extends Component {
   render() {
     return (      
-       <BrowserRouter>
-        <div>
-          <Navigation />
-            <Switch>
-             <Route path="/" component={Home} exact/>
-             <Route path="/blog" component={Blog} />
-             <Route path="/about" component={About}/>
-             <Route path="/contact" component={Contact}/> 
-           </Switch>
-        </div> 
-      </BrowserRouter>
+        <React.Fragment>
+                <BrowserRouter>
+                   <div>
+                       <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/blog" component={Blog} />
+                            <Route path="/about" component={About}/>
+                            <Route path="/contact" component={Contact}/> 
+                      </Switch>
+                    </div>
+                </BrowserRouter>
+        </React.Fragment> 
     );
   }
 }
