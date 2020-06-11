1344b1eb3277b2db588b934397c117cab9082d13
untari <sastraembun0306@gmail.com>
2020-06-11 15:00:01 +0700

    configuring main page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css'; 
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Blog from './Components/Blog';
+import Navigation from './Components/Navigation';
+ 
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/blog" component={Blog} />
+          {/*   <Route path="/about" component={About}/>*/}
+           {/*  <Route path="/contact" component={Contact}/> */}
+           </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
 }
-
+ 
 export default App;
