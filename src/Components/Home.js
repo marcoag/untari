b66eddfaf0ec6c415cba72698fc743da738e2b4b
untari <sastraembun0306@gmail.com>
2020-06-12 16:01:46 +0700

    updating index.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,16 +3,18 @@ import { NavLink } from 'react-router-dom'
 
 const Home = () => {
     return (
-       <div>
-            <h3 className="masthead-brand">Untari.com</h3>
+        <div className=".bg">
+            <div>
+                <h3 className="masthead-brand">Untari.com</h3>
                 <nav>
-                <ul className="nav masthead-nav">
-                    <li className="active"><a href="#Home">Home</a></li>
-                    <li><NavLink to="About">About</NavLink></li>
-                    <li><NavLink to="Contact">Contact</NavLink></li>
-                    <li><NavLink to="Blog">Blog</NavLink></li>
-                </ul>
+                    <ul className="nav masthead-nav">
+                        <li className="active"><a href="#Home">Home</a></li>
+                        <li><NavLink to="About">About</NavLink></li>
+                        <li><NavLink to="Contact">Contact</NavLink></li>
+                        <li><NavLink to="Blog">Blog</NavLink></li>
+                    </ul>
                 </nav>
+            </div>
        </div>
     );
 }
