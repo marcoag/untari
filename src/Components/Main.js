1f21c3ee0785da02b4f80f6d7ecba883784f1793
untari <sastraembun0306@gmail.com>
2020-06-12 13:46:20 +0700

    modified the button

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,8 +7,10 @@ const Main = () => {
             <div className="site-wrapper">
                <div className="site-wrapper-inner">
                 <div className="inner cover">
-                <h1 className="inner-heading">Welcome to Untari personal website</h1>
-                <button type="button" className="btn btn-info"><NavLink to="Home">Click here to enter</NavLink></button>
+                    <h1 className="inner-heading">Welcome to Untari personal website</h1>
+                    <p className="lead">
+                        <button type="button" className=" btn btn-success"><NavLink to="Home">Click here to enter</NavLink></button>
+                    </p>
                 </div>
               </div>
             </div>
