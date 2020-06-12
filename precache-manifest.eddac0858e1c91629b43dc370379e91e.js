a59e8326c04412e92f31204021b1c97edc7bcaa1
untari <sastraembun0306@gmail.com>
2020-06-12 12:17:32 +0700

    Updates

---
+++ precache-manifest.eddac0858e1c91629b43dc370379e91e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef812fa953ed37fa87e32287dd7282c0",
+    "url": "/untari.com/index.html"
+  },
+  {
+    "revision": "e5493b2dbd792c3731ba",
+    "url": "/untari.com/static/css/main.8417ee23.chunk.css"
+  },
+  {
+    "revision": "5165478c450c8c747208",
+    "url": "/untari.com/static/js/2.ed3f0902.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/untari.com/static/js/2.ed3f0902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5493b2dbd792c3731ba",
+    "url": "/untari.com/static/js/main.730a8932.chunk.js"
+  },
+  {
+    "revision": "df9fd43cf6d15c8eae2a",
+    "url": "/untari.com/static/js/runtime-main.4b9efd96.js"
+  }
+]);
